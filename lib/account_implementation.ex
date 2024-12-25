f0634b849396b30dbecd75ff8b8af2e792958259
iyjim <jim.chang@ironyun.com>
2024-12-25 16:29:08 +0800

    use correct syntax

--- lib/account_implementation.ex
+++ lib/account_implementation.ex
@@ -2,21 +2,15 @@ defmodule AccountImplementation do
   use Ash.Type.NewType,
     subtype_of: :union,
     constraints: [
-      checking: [
-        type: :struct,
-        constraints: [instance_of: CheckingAccount]
-      ],
-      savings: [
-        type: :struct,
-        constraints: [instance_of: SavingsAccount]
-      ]
-      # types: [
-      #   checking: [
-      #     type: CheckingAccount
-      #   ],
-      #   savings: [
-      #     type: SavingsAccount
-      #   ]
-      # ]
+      types: [
+        checking: [
+          type: :struct,
+          constraints: [instance_of: CheckingAccount]
+        ],
+        savings: [
+          type: :struct,
+          constraints: [instance_of: SavingsAccount]
+        ]
+      」
     ]
 end
